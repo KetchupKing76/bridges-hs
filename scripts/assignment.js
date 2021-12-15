e83f52119313792139aeb8a72280bf238e84f20b
UnaryPlus <ombspring@gmail.com>
2021-12-15 15:51:56 -0500

    fix relative links

--- scripts/assignment.js
+++ scripts/assignment.js
@@ -388,7 +388,7 @@ function mountainPathsDescription() {
   );
 
   const diagram = (num, direction) => figure(
-    img({ src : '../images/mountain-paths/greedy-case-' + num + '.png', alt : 'Case ' + num }),
+    img({ src : '/bridges-hs/images/mountain-paths/greedy-case-' + num + '.png', alt : 'Case ' + num }),
     figcaption('Case ' + num + ': ' + direction)
   );
 
