5035271fd5378958465ec92481c63161d8c7f3e4
UnaryPlus <ombspring@gmail.com>
2022-07-14 13:40:56 -0400

    add filter bar

---
+++ assignments/search.js
@@ -0,0 +1,30 @@
+---
+---
+
+const searchData = {
+  {% for asst in site.data.assignments %}
+  "{{ asst.key }}" : `
+    {{ asst.title }} {{ asst.summary }} {{ asst.description }}
+    {% for tag in asst.tags %} {{ tag }} {% endfor %}
+    {% for topic in asst.topics %} {{ topic }} {% endfor %}
+    {% for class in asst.documentation %} {{ class }} {% endfor %}
+  `,
+  {% endfor %}
+}
+
+function normalise(str) {
+  return str.trim().replace(/\s+/g, " ").toLowerCase()
+}
+
+for(const key in searchData) {
+  searchData[key] = normalise(searchData[key])
+}
+
+document.getElementById("search").oninput = function() {
+  const terms = normalise(this.value)
+  for(const key in searchData) {
+    document.getElementById(key).style.display =
+      searchData[key].includes(terms)
+      ? '' : 'none'
+  }
+}
